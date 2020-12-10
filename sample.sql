c79de6b2ba3130c9b5896a7b21da3dc02c568295
preethika <preethikathiru05@gmail.com>
2020-12-10 09:33:53 +0000

    added 2 queries

--- sample.sql
+++ sample.sql
@@ -1,1 +1,19 @@
-select * from students;
+
+1) To display students with max package:
+
+select s.NAME,c.NAME,p.PACKAGE from placements p
+INNER JOIN student s on (s.ID = p.S_ID)
+INNER JOIN company c on (c.ID = p.C_ID)
+where PACKAGE = (select max(PACKAGE) from  placements);
+
+2) TO display ECE students placements details:
+
+select s.NAME , s.DEPARTMENT , c.NAME , p.PACKAGE,
+CASE
+ 	when p.S_ID is NULL then "NO"
+ 	ELSE "YES"
+ END AS PLACED
+ from placements p
+RIGHT JOIN student s ON (s.ID = p.S_ID)
+LEFT JOIN company c ON (c.ID = p.C_ID) 
+WHERE s.DEPARTMENT = 'ECE';
